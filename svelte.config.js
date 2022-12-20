56d977facd6a30fc7cbcab1691b462ce8529ed7f
hexcavator <btdepuy@gmail.com>
2022-12-20 17:18:58 -0500

    testing static netlify adapter v5

--- svelte.config.js
+++ svelte.config.js
@@ -5,7 +5,6 @@ import preprocess from "svelte-preprocess";
 const config = {
 	kit: {
 		adapter: adapter({ precompress: true }),
-    prerender: { default: true },
 	},
 	preprocess: [
 		preprocess({
