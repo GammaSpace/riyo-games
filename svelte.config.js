0ae0064a3b92d048242b98318bb53029aef08978
hexcavator <btdepuy@gmail.com>
2022-12-20 17:03:16 -0500

    testing static netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,16 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-netlify';
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+    prerender: { 
+			crawl: true, 
+			enabled: true, 
+			onError: 'continue', 
+			pages: ['*'] 
+		},
 	},
 	preprocess: [
 		preprocess({
