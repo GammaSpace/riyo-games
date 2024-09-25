06fabd222afa43312b0a0620a3fcb592b6ea3b89
Jennie Robinson Faber <jennie@jenniefaber.com>
2024-09-25 09:13:27 -0400

    Set max page width

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,105 +1,179 @@
 <script>
-  import "../app.css";
-  import { page } from '$app/stores';
-  import { render as htmlRender } from 'datocms-structured-text-to-html-string';
+	import '../app.css';
+	import { page } from '$app/stores';
+	import { render as htmlRender } from 'datocms-structured-text-to-html-string';
 
-  export let data;
-  const currentYear = new Date().getFullYear();
-  const pages = [
-    ["home","/"],    
-    ["about","/about"],
-    ["careers","/careers"],
-    ["games","https://threadsoftimegame.com"],
-    ["contact","/contact"]
-  ]
+	export let data;
+	const currentYear = new Date().getFullYear();
+	const pages = [
+		['home', '/'],
+		['about', '/about'],
+		['careers', '/careers'],
+		['games', 'https://threadsoftimegame.com'],
+		['contact', '/contact']
+	];
 
-  let yPos = 0;
+	let yPos = 0;
 
-  let logoImg = "/img/logos/riyo_games_logo_586x288.png";
-  let navTabImg = "/img/ui/nav_logo_tab.png";
-  let navSelectedImg = "/img/ui/nav_selected_state.png";
-  let ruleOfLaw = "/img/ui/rule_of_law.png";
+	let logoImg = '/img/logos/riyo_games_logo_586x288.png';
+	let navTabImg = '/img/ui/nav_logo_tab.png';
+	let navSelectedImg = '/img/ui/nav_selected_state.png';
+	let ruleOfLaw = '/img/ui/rule_of_law.png';
 
-  let winWidth = 0;
-  let navSelected = $page.url.pathname;
+	let winWidth = 0;
+	let navSelected = $page.url.pathname;
 
-  let showDropdown = false;
-  function mobileMenu() {
-    showDropdown = !showDropdown;
-  }
+	let showDropdown = false;
+	function mobileMenu() {
+		showDropdown = !showDropdown;
+	}
 
-  $: navSelected = $page.url.pathname;  
+	$: navSelected = $page.url.pathname;
 </script>
 
-<svelte:window bind:scrollY={yPos} bind:innerWidth={winWidth}/>
+<svelte:window bind:scrollY={yPos} bind:innerWidth={winWidth} />
 
-<div class="bg-transparent flex flex-wrap">
-  <div class="z-[100] block md:hidden header-fade fixed { yPos > 0 && !showDropdown ? "header-gradient" : "bg-transparent"} w-full h-[60px]">
-    <div class="z-[100] block md:hidden absolute left-8 top-0"><img alt="Riyo Games Logo" class="w-[50px] h-auto" src={navTabImg}/></div>
-    <div on:click={()=>mobileMenu()} class="z-[90] block md:hidden text-charcoal h-[48px] w-[48px] -right-1 fixed hover:cursor-pointer">
-      <div class="border-2 w-[32px] border-charcoal rounded mt-2"></div>
-      <div class="border-2 w-[32px] border-charcoal rounded mt-2"></div>
-      <div class="border-2 w-[32px] border-charcoal rounded mt-2"></div>
-    </div>
-  </div>
-  <div
-    class={`smooth-menu z-[90] block md:hidden text-center w-full fixed overflow-hidden bg-whiteTan text-white ${showDropdown ? "max-h-150" : "max-h-0"}`}>
-    <div class="pt-16 pb-4">
-      {#each pages as page}
-        <div on:click={()=>mobileMenu()} class="text-center">
-          <a class="py-4 text-textGray font-menu uppercase text-lg" href="{ page[1] }">{ page[0] }</a>
-            <img alt="Horizontal Rule" class="my-2 h-[1.5px] object-left mx-auto w-[40%]" src={ruleOfLaw} />
-        </div>
-      {/each}
-      <div class="py-4 text-center">
-        <div class="inline-block">
-          { #each data.allSocials as social }
-            <a on:click={()=>mobileMenu()} class="mx-3" href={social.link}><img alt="Social Media Icon" class="inline-block w-[45px] h-auto" src={social.svg.url}/></a>
-          {/each}
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="hidden md:block header-fade z-40 fixed p-4 { yPos > 0 ? "header-gradient" : "bg-transparent"} w-full h-[80px]">
-    <div class="absolute left-8 top-0"><img alt="Riyo Games Logo" class="w-[50px] h-auto" src={navTabImg}/></div>
-    <div class="absolute right-8">
-      { #each pages as page }
-        <span class="relative pl-4">
-          <h4 class="inline-block uppercase { navSelected == page[1] ? "text-boxBlue" : "" } hover:text-boxBlue"><a href="{ page[1] }">{ page[0] }</a></h4>
-          { #if navSelected == page[1] }
-            <div class="absolute -top-[19px] left-1/2"><img alt="Navigation Selector" class="mx auto w-[70%]" src={navSelectedImg}/></div>
-          { /if }
-        </span>
-      { /each }
-      <div class="pl-4 inline-block">
-        { #each data.allSocials as social }
-          <a href={social.link}><img alt="Social Media Icon" class="mx-1 inline-block w-[30px] h-auto" src={social.svg.url}/></a>
-        {/each}
-      </div>
-    </div>
-  </div>
-  <div class="w-full min-h-[calc(100vh-80px)]">
-    <slot />
-  </div>
-  <div class="w-full relative">
-    { #if navSelected != "/about" && navSelected != "/games" }
-      <div class="z-20 w-full bottom-[90px] absolute h-[80px] footer-gradient"></div>
-    { /if }
-    <div class="z-30 mission-mask w-full h-[100px] flex flex-wrap items-center relative -mt-8">
-      <div class="absolute bg-tan bg-center md:bg-cover bg-no-repeat w-full h-full"></div>
-      <div class="pt-2 px-8 absolute w-full flex flex-wrap items-center">
-        <div class="w-full text-left flex flex-wrap items-center">      
-          <img alt="Riyo Games Logo" class="mb-0 mt-1 h-[30px]" src={logoImg}/>
-          { #if winWidth > 768 }
-            <span class="font-body text-textGray text-tiny pl-2">&#169; Riyo Inc {currentYear}, All Rights Reserved |</span><span class="style-dato-footer">{@html htmlRender( data.footer.footerContent ) }</span>   
-          { /if }
-        </div>
-        { #if winWidth <= 768 }
-          <div class="w-full">
-            <span class="py-2 font-body text-textGray text-tiny">&#169; Riyo Inc {currentYear}, All Rights Reserved |</span><span class="style-dato-footer">{@html htmlRender( data.footer.footerContent ) }</span>   
-          </div>
-        { /if }
-      </div>
-    </div>
-  </div>
+<div class="flex flex-wrap mx-auto max-w-10xl relative">
+	<!-- Mobile Header -->
+	<div
+		class="z-[100] block md:hidden header-fade fixed {yPos > 0 && !showDropdown
+			? 'header-gradient'
+			: 'bg-transparent'} w-full h-[60px]"
+	>
+		<div class="z-[100] block md:hidden absolute left-8 top-0">
+			<img alt="Riyo Games Logo" class="w-[50px] h-auto" src={navTabImg} />
+		</div>
+		<div
+			on:click={() => mobileMenu()}
+			class="z-[90] block md:hidden text-charcoal h-[48px] w-[48px] -right-1 fixed hover:cursor-pointer"
+		>
+			<div class="border-2 w-[32px] border-charcoal rounded mt-2" />
+			<div class="border-2 w-[32px] border-charcoal rounded mt-2" />
+			<div class="border-2 w-[32px] border-charcoal rounded mt-2" />
+		</div>
+	</div>
+
+	<!-- Mobile Dropdown Menu -->
+	<div
+		class={`smooth-menu z-[90] block md:hidden text-center w-full fixed overflow-hidden bg-whiteTan text-white ${
+			showDropdown ? 'max-h-150' : 'max-h-0'
+		}`}
+	>
+		<div class="pt-16 pb-4">
+			{#each pages as page}
+				<div on:click={() => mobileMenu()} class="text-center">
+					<a class="py-4 text-textGray font-menu uppercase text-lg" href={page[1]}>{page[0]}</a>
+					<img
+						alt="Horizontal Rule"
+						class="my-2 h-[1.5px] object-left mx-auto w-[40%]"
+						src={ruleOfLaw}
+					/>
+				</div>
+			{/each}
+			<div class="py-4 text-center">
+				<div class="inline-block">
+					{#each data.allSocials as social}
+						<a on:click={() => mobileMenu()} class="mx-3" href={social.link}
+							><img
+								alt="Social Media Icon"
+								class="inline-block w-[45px] h-auto"
+								src={social.svg.url}
+							/></a
+						>
+					{/each}
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Desktop Header with Navigation (Overlay) -->
+	<div class="hidden md:block fixed z-50 w-full max-w-10xl mx-auto left-1/2 -translate-x-1/2 top-0">
+		<div class="flex justify-between items-center pl-12">
+			<div class="flex items-center">
+				<img alt="Riyo Games Logo" class="w-[50px] h-auto" src={navTabImg} />
+			</div>
+			<div class="flex items-center">
+				{#each pages as page}
+					<span class="relative px-4">
+						<h4
+							class="inline-block uppercase {navSelected == page[1]
+								? 'text-boxBlue'
+								: 'text-stone-800'} hover:text-boxBlue transition-colors"
+						>
+							<a href={page[1]}>{page[0]}</a>
+						</h4>
+						{#if navSelected == page[1]}
+							<div class="absolute -top-[19px] left-1/2 -translate-x-1/2">
+								<img alt="Navigation Selector" class="mx-auto w-[70%]" src={navSelectedImg} />
+							</div>
+						{/if}
+					</span>
+				{/each}
+				<div class="pl-4 inline-block">
+					{#each data.allSocials as social}
+						<a href={social.link}
+							><img
+								alt="Social Media Icon"
+								class="mx-1 inline-block w-[30px] h-auto"
+								src={social.svg.url}
+							/></a
+						>
+					{/each}
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Gradient Header Background -->
+	<div
+		class="hidden md:block header-fade z-40 fixed {yPos > 0
+			? 'header-gradient'
+			: 'bg-transparent'} w-full max-w-10xl mx-auto left-1/2 -translate-x-1/2 h-[80px] pointer-events-none"
+	/>
+
+	<!-- Main Content -->
+	<div class="w-full min-h-screen">
+		<slot />
+	</div>
+
+	<!-- Footer -->
+	<div class="w-full relative">
+		{#if navSelected != '/about' && navSelected != '/games'}
+			<div class="z-20 w-full bottom-[90px] absolute h-[80px] footer-gradient" />
+		{/if}
+		<div class="z-30 mission-mask w-full h-[100px] flex flex-wrap items-center relative -mt-8">
+			<div class="absolute bg-tan bg-center md:bg-cover bg-no-repeat w-full h-full" />
+			<div class="pt-2 px-8 absolute w-full flex flex-wrap items-center">
+				<div class="w-full text-left flex flex-wrap items-center">
+					<img alt="Riyo Games Logo" class="mb-0 mt-1 h-[30px]" src={logoImg} />
+					{#if winWidth > 768}
+						<span class="font-body text-textGray text-tiny pl-2"
+							>&#169; Riyo Inc {currentYear}, All Rights Reserved |</span
+						><span class="style-dato-footer">{@html htmlRender(data.footer.footerContent)}</span>
+					{/if}
+				</div>
+				{#if winWidth <= 768}
+					<div class="w-full">
+						<span class="py-2 font-body text-textGray text-tiny"
+							>&#169; Riyo Inc {currentYear}, All Rights Reserved |</span
+						><span class="style-dato-footer">{@html htmlRender(data.footer.footerContent)}</span>
+					</div>
+				{/if}
+			</div>
+		</div>
+	</div>
 </div>
+
+<style>
+	.header-gradient {
+		background: linear-gradient(
+			to bottom,
+			rgba(255, 255, 255, 0.8) 0%,
+			rgba(255, 255, 255, 0) 100%
+		);
+	}
+
+	.footer-gradient {
+		background: linear-gradient(to top, rgba(255, 255, 255, 0.8) 0%, rgba(255, 255, 255, 0) 100%);
+	}
+</style>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,41 +1,46 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        menu: [ "Quadraat", "Helvetica", "Arial" ],
-        body: [ "Volkhov", "Helvetica", "Arial" ],
-      },
-      fontSize: {
-        tiny: ['12px', '16px'],
-        sm: ['14px', '20px'],
-        base: ['16px', '24px'],
-        md: ['18px', '28px'],
-        lg: ['20px', '30px'],
-        xl: ['24px', '32px'],
-      },
-      colors: {
-        beige: "#e3d7bb",
-        tan: "#ede6d7",
-        whiteTan: "#fffdf8",
-        darkBeige: "#b3a78b",
-        midBeige: "#d3c7ab",
-        charcoal: "#1f1e1d",
-        lightCharcoal: "#5f5e5d",
-        textGray: "#716c61",
-        boxBlue: "#0b93e7",
-      },
-      backgroundImage: {
-        welcomeBandBg: "url('/img/home/welcome_band_background_better.jpg')",
-        missionBandBg: "url('/img/home/mission_band_background.jpg')",
-        aboutBandMidBg: "url('/img/about/about_band_middle.jpg')",
-        aboutBandBottomBg: "url('/img/about/about_band_bottom.jpg')",
-        totBandBg: "url('/img/home/tot_band_background.jpg')",
-        careersBandTopBg: "url('/img/careers/careers_band_top.jpg')",
-        careersBandMidBg: "url('/img/careers/careers_band_middle.jpg')",
-      }
-    }
-  },
-  plugins: []
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			maxWidth: {
+				'8xl': '88rem',
+				'9xl': '96rem',
+				'10xl': '104rem'
+			},
+			fontFamily: {
+				menu: ['Quadraat', 'Helvetica', 'Arial'],
+				body: ['Volkhov', 'Helvetica', 'Arial']
+			},
+			fontSize: {
+				tiny: ['12px', '16px'],
+				sm: ['14px', '20px'],
+				base: ['16px', '24px'],
+				md: ['18px', '28px'],
+				lg: ['20px', '30px'],
+				xl: ['24px', '32px']
+			},
+			colors: {
+				beige: '#e3d7bb',
+				tan: '#ede6d7',
+				whiteTan: '#fffdf8',
+				darkBeige: '#b3a78b',
+				midBeige: '#d3c7ab',
+				charcoal: '#1f1e1d',
+				lightCharcoal: '#5f5e5d',
+				textGray: '#716c61',
+				boxBlue: '#0b93e7'
+			},
+			backgroundImage: {
+				welcomeBandBg: "url('/img/home/welcome_band_background_better.jpg')",
+				missionBandBg: "url('/img/home/mission_band_background.jpg')",
+				aboutBandMidBg: "url('/img/about/about_band_middle.jpg')",
+				aboutBandBottomBg: "url('/img/about/about_band_bottom.jpg')",
+				totBandBg: "url('/img/home/tot_band_background.jpg')",
+				careersBandTopBg: "url('/img/careers/careers_band_top.jpg')",
+				careersBandMidBg: "url('/img/careers/careers_band_middle.jpg')"
+			}
+		}
+	},
+	plugins: []
 };
